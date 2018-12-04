863db1fdc850d58df6767a1386bf676a0a5a6306
ErfanMozaffari <erfanmozaffari@yahoo.com>
2018-12-04 12:37:02 +0330

    test

--- BraidedPaths/unlockFolders.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# make all files and folders unlocked
-currentUser=`who | awk '{print $1}'`
-path="../${dirName[0]}"
-# args:   "openwsn" "../results"
-chown -R $currentUser: "results"
